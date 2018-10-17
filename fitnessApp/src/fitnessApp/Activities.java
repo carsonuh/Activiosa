b4a055f1f14e017599744371663cc4eab3eb7d82
carsonuh <carsonueckerherman@gmail.com>
2018-10-17 14:41:06 -0400

    Added Activities

---
+++ fitnessApp/src/fitnessApp/Activities.java
@@ -0,0 +1,7 @@
+package fitnessApp;
+
+public interface Activities {
+    void duration(int time);
+    void calsBurned(int cals);
+}
+
--- fitnessApp/src/fitnessApp/GUI.java
+++ fitnessApp/src/fitnessApp/GUI.java
@@ -1,6 +1,6 @@
 package fitnessApp;
 
-public class main {
+public class GUI {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
